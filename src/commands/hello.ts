039a2f982fb8591b08ed5f6fd570dba871bc03bc
yamenw <yamentaad@gmail.com>
2023-07-14 17:11:10 +0300

    no clue what i am doing

---
+++ src/commands/hello.ts
@@ -0,0 +1,13 @@
+export function hello(data: any) {
+    const { value } = data?.options?.find(
+        (option: { name: string; value: string }) => option.name === 'name'
+      )
+      return json({
+        // Type 4 responds with the below message retaining the user's
+        // input at the top.
+        type: 4,
+        data: {
+          content: `Hello, ${value}!`,
+        },
+      })
+}
---
+++ src/types/commands.ts
@@ -0,0 +1,4 @@
+export enum DiscordCommandType {
+    Ping = 1,
+    ApplicationCommand = 2,
+}
