4ccb939ac1b834fdbaf88cfcbd2b7968241e16f0
yamenw <yamentaad@gmail.com>
2023-07-14 21:00:21 +0300

    minor code clean up

--- src/home.ts
+++ src/home.ts
@@ -3,73 +3,55 @@ import * as sift from 'sift';
 import { DiscordCommandType } from './types/commands.ts';
 import { hello } from './commands/hello.ts';
 
-
-
-
-
-// The main logic of the Discord Slash Command is defined in this function.
 export async function home(request: Request) {
-    // validateRequest() ensures that a request is of POST method and
-    // has the following headers.
     const { error } = await sift.validateRequest(request, {
-      POST: {
-        headers: ['X-Signature-Ed25519', 'X-Signature-Timestamp'],
-      },
+        POST: {
+            headers: ['X-Signature-Ed25519', 'X-Signature-Timestamp'],
+        },
     })
     if (error) {
-      return sift.json({ error: error.message }, { status: error.status })
+        return sift.json({ error: error.message }, { status: error.status })
     }
-  
-    // verifySignature() verifies if the request is coming from Discord.
-    // When the request's signature is not valid, we return a 401 and this is
-    // important as Discord sends invalid requests to test our verification.
+
     const { valid, body } = await verifySignature(request)
     if (!valid) {
-      return sift.json(
-        { error: 'Invalid request' },
-        {
-          status: 401,
-        }
-      )
+        return sift.json(
+            { error: 'Invalid request' },
+            {
+                status: 401,
+            }
+        )
     }
-  
+
     const { type = 0, data = { options: [] } } = JSON.parse(body)
-    // Discord performs Ping interactions to test our application.
-    // Type 1 in a request implies a Ping interaction.
-    if (type === DiscordCommandType.Ping) {
-      return sift.json({
-        type: 1, // Type 1 in a response is a Pong interaction response type.
-      })
+    switch (type) {
+        case DiscordCommandType.Ping:
+            return sift.json({
+                type: 1,
+            })
+
+        case DiscordCommandType.ApplicationCommand:
+            return hello(data);
+
+        default:
+            return sift.json({ error: 'ETYPEZERO: bad request' }, { status: 400 })
     }
-  
-    // Type 2 in a request is an ApplicationCommand interaction.
-    // It implies that a user has issued a command.
-    if (type === DiscordCommandType.ApplicationCommand) {
-      return hello(data);
-    }
-  
-    // We will return a bad request error as a valid Discord request
-    // shouldn't reach here.
-    return sift.json({ error: 'bad request' }, { status: 400 })
-  }
-  
-  /** Verify whether the request is coming from Discord. */
-  async function verifySignature(request: Request): Promise<{ valid: boolean; body: string }> {
+}
+
+async function verifySignature(request: Request): Promise<{ valid: boolean; body: string }> {
     const PUBLIC_KEY = Deno.env.get('DISCORD_PUBLIC_KEY')!
-    // Discord sends these headers with every request.
     const signature = request.headers.get('X-Signature-Ed25519')!
     const timestamp = request.headers.get('X-Signature-Timestamp')!
     const body = await request.text()
     const valid = nacl.sign.detached.verify(
-      new TextEncoder().encode(timestamp + body),
-      hexToUint8Array(signature),
-      hexToUint8Array(PUBLIC_KEY)
+        new TextEncoder().encode(timestamp + body),
+        hexToUint8Array(signature),
+        hexToUint8Array(PUBLIC_KEY)
     )
-  
+
     return { valid, body }
-  }
-  
-  /** Converts a hexadecimal string to Uint8Array. */
-  function hexToUint8Array(hex: string) {
+}
+
+function hexToUint8Array(hex: string) {
     return new Uint8Array(hex.match(/.{1,2}/g)!.map((val) => parseInt(val, 16)))
-  }
+}
