f9370c01863ef8b4746681c9aebc452b19fab342
yamenw <yamentaad@gmail.com>
2023-07-22 15:50:38 +0300

    add zod for validation

---
+++ src/schema/schema.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+
+const registerData = z.object({
+    options: z.array(z.object({
+        type: z.literal(3),
+        name: z.literal("handle"),
+        value: z.string().min(1)
+    })),
+    name: z.literal('register')
+})
+
+export type RegisterData = z.infer<typeof registerData>;
+
+export const dataSchema = z.discriminatedUnion('name', [
+    z.object({
+        options: z.array(z.any()),
+        name: z.literal('hello')
+    }),
+    registerData
+])
+
+export const interactionSchema = z.object({
+    type: z.number().min(1).max(3).int(),
+    data: dataSchema
+})
+
+export type InteractionData = z.infer<typeof dataSchema>;
