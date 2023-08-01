6b6b2948bdaf740180229d5a944abd7d14cc18a1
yamenw <yamentaad@gmail.com>
2023-08-02 01:03:22 +0300

    add get option helper

--- src/helper/get-option.ts
+++ src/helper/get-option.ts
@@ -1,11 +1,11 @@
-export function getOption(name: string, data: { name: string; value: string }[]) {
+export function getOption(name: string, data: { name: string; value: string | number }[]) {
     const option = data?.find(
-        (option: { name: string; value: string }) => option.name === name
+        (option: { name: string; value: string | number }) => option.name === name
     )
     if (!option)
         return undefined;
     // throw new Error(`Missing option: ${name}`);
-    return option;
+    return option.value;
 }
 
 //TODO: TS wizardry with generics
