26d99f67e63deeb41e25d06122db8f71a68060a3
yamenw <yamentaad@gmail.com>
2023-08-04 15:04:41 +0300

    add rest API function

---
+++ supabase/functions/rest-api/index.ts
@@ -0,0 +1,16 @@
+import { serve } from "http"
+
+console.log("Hello from Functions!")
+
+serve(async (req) => {
+  const { name } = await req.json()
+  const data = {
+    message: `Hello ${name}!`,
+  }
+
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
+
