8ff23065f1123f880b979f71a9f652f619a1f3ac
yamenw <yamentaad@gmail.com>
2024-07-06 18:35:44 +0300

    fix broken path in rest API

--- supabase/functions/rest-api/index.ts
+++ supabase/functions/rest-api/index.ts
@@ -1,5 +1,6 @@
-import { serve } from "http"
-import { resolveREST } from "../../../src/api/index.ts";
+import { serve } from "http";
+import { resolveREST } from "../_shared/api/index.ts";
+
 
 serve(async (req) => {
   return await resolveREST(req);
