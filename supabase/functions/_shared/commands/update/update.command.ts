a599408e5e56ba63b2eb47f21c1a73200e86417d
yamenw <yamentaad@gmail.com>
2024-07-06 16:10:15 +0300

    #2: fix update command being not up to date

--- supabase/functions/_shared/commands/update/update.command.ts
+++ supabase/functions/_shared/commands/update/update.command.ts
@@ -1,4 +1,38 @@
 export const updateCommandDefinition = {
-    "name": "update",
-    "description": "Update your problemlist based on your handle"
+    name: "update",
+    description: "Update your problemlist based on your handle",
+    options: [
+        {
+            "name": "set_nickname",
+            "description": "Optional nickname for you in place of the CF handle",
+            "type": 3,
+            "required": false,
+            "min_length": 1,
+            "max_length": 32
+        },
+        {
+            "name": "refetch_last",
+            "description": "How many submissions since your first submission to skip when fetching, for debugging",
+            "type": 4,
+            "required": false,
+            "choices": [
+                {
+                    "name": "last 25 submissions",
+                    "value": 25
+                },
+                {
+                    "name": "last 50 submissions",
+                    "value": 50
+                },
+                {
+                    "name": "last 100 submissions",
+                    "value": 100
+                },
+                {
+                    "name": "last 250 submissions",
+                    "value": 250
+                }
+            ]
+        }
+    ]
 } as const;
